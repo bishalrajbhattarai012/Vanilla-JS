9e27263011bc65d914c644cf61a3b0c8a7be5aa3
bishalrajbhattarai012 <bishal.bhattara.ebpearls@gmail.com>
2025-05-20 14:15:39 +0545

    Color Palette

---
+++ typescript/src/color.ts
@@ -0,0 +1,75 @@
+const container = <HTMLDivElement>document.querySelector("#paletteContainer");
+const generateBtn = <HTMLButtonElement>document.querySelector("#generateBtn");
+const favLists = <HTMLDivElement>document.querySelector("#favoriteList");
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    const colors = JSON.parse(localStorage.getItem("colors") || "[]");
+    colors.forEach((hex: string) => {
+      const div = document.createElement("div");
+      div.classList.add("colorBox");
+      div.textContent = hex;
+      div.style.backgroundColor = hex;
+      favLists.append(div);
+    });
+  });
+
+
+
+
+function getRandomHexColor(): string {
+    const hex = Math.floor(Math.random() * 0xffffff).toString(16);
+    return '#' + ('000000' + hex).slice(-6);
+  }
+
+
+document.body.addEventListener("click", (e: Event) => {
+  const target = <HTMLElement>e.target;
+  if (target.matches("#generateBtn")) {
+    const palette = [getRandomHexColor(), getRandomHexColor(), getRandomHexColor(), getRandomHexColor()];
+    palette.forEach((color:string)=>{
+        const div = document.createElement("div")
+            div.classList.add("colorBox") 
+            div.textContent= color
+            div.style.backgroundColor=color
+            container.append(div)
+            
+
+
+
+  const favBtn = document.createElement("button");
+  favBtn.classList.add("fav")
+  favBtn.textContent = "❤️";
+  favBtn.title = "Add to Favorites";
+  favBtn.style.cursor = "pointer";
+  favBtn.style.fontSize = "10px";
+        div.append(favBtn)
+
+    })
+  }
+
+
+
+  if(target.matches(".fav")){
+    const favButton = <HTMLButtonElement> target
+    const parentDiv = <HTMLDivElement>favButton.parentElement
+    const text = parentDiv.textContent
+    const colorHex = parentDiv.textContent?.slice(0,text!.length-2) || "";
+    const favColors = JSON.parse(localStorage.getItem("color") || "[]")
+    favColors.push(colorHex)
+
+
+}
+
+
+  if(target.matches(".colorBox")){
+    const box = <HTMLDivElement>target
+    const text :string = box.textContent?.trim() || ""
+    const colorHex = box.textContent?.slice(0,text.length-2) || ""
+    console.log(colorHex);
+
+    navigator.clipboard.writeText(colorHex)
+    .catch((err) => console.error("Failed to copy: ", err))
+    
+  }
+});
--- typescript/src/generics.ts
+++ typescript/src/generics.ts
@@ -1,86 +1,158 @@
-const addStuff = <T extends number | string>(arg1: T, arg2: T) => {
-  if (typeof arg1 == "number" && typeof arg2 == "number") return arg1 + arg2;
+// const addStuff = <T extends number | string>(arg1: T, arg2: T) => {
+//   if (typeof arg1 == "number" && typeof arg2 == "number") return arg1 + arg2;
 
-  if (typeof arg1 == "string" || typeof arg2 == "string")
-    return "" + arg1 + arg2;
-  else throw new Error("invalid Data Type cannot process");
+//   if (typeof arg1 == "string" || typeof arg2 == "string")
+//     return "" + arg1 + arg2;
+//   else throw new Error("invalid Data Type cannot process");
+// };
+
+// try {
+//   const result = addStuff("2", "3");
+// } catch (e: any) {
+//   console.error(`ERROR: ${e.message}`);
+// }
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "Bishal Sharma",
+//     email: "bishal@example.com",
+//     profile: {
+//       age: 25,
+//       gender: "male",
+//       location: {
+//         city: "Kathmandu",
+//         country: "Nepal",
+//       },
+//     },
+//     preferences: {
+//       theme: "dark",
+//       notifications: {
+//         email: true,
+//         sms: false,
+//       },
+//     },
+//   },
+//   {
+//     id: 2,
+//     name: "Sita Rai",
+//     email: "sita@example.com",
+//     profile: {
+//       age: 28,
+//       gender: "female",
+//       location: {
+//         city: "Pokhara",
+//         country: "Nepal",
+//       },
+//     },
+//     preferences: {
+//       theme: "light",
+//       notifications: {
+//         email: false,
+//         sms: true,
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     name: "John Doe",
+//     email: "john.doe@example.com",
+//     profile: {
+//       age: 30,
+//       gender: "other",
+//       location: {
+//         city: "Lalitpur",
+//         country: "Nepal",
+//       },
+//     },
+//     preferences: {
+//       theme: "system",
+//       notifications: {
+//         email: true,
+//         sms: true,
+//       },
+//     },
+//   },
+// ];
+
+// type User = typeof users[0];
+
+// const getUsersProperty = <T extends User, K extends keyof T> (users:T[], key: K)  =>{
+//     return users.map((user)=>(user[key]))
+// }
+
+// const usersOfOnlyKey = getUsersProperty(users,"name")
+// console.log(usersOfOnlyKey);
+
+// challenge 1
+
+const wrapInArray = <T>(arg: T): [T] => {
+  return [arg];
 };
 
-try {
-  const result = addStuff("2", "3");
-  //  console.log(result)
-} catch (e: any) {
-  console.error(`ERROR: ${e.message}`);
+/*
+{
+name
+age
+class
+}
+*/
+
+const gP = <T, K extends keyof T>(o: T, k: K): T[K] => o[k];
+
+interface IStudent {
+  name: string;
+  age: number;
 }
 
-const users = [
-  {
-    id: 1,
-    name: "Bishal Sharma",
-    email: "bishal@example.com",
-    profile: {
-      age: 25,
-      gender: "male",
-      location: {
-        city: "Kathmandu",
-        country: "Nepal",
-      },
-    },
-    preferences: {
-      theme: "dark",
-      notifications: {
-        email: true,
-        sms: false,
-      },
-    },
-  },
-  {
-    id: 2,
-    name: "Sita Rai",
-    email: "sita@example.com",
-    profile: {
-      age: 28,
-      gender: "female",
-      location: {
-        city: "Pokhara",
-        country: "Nepal",
-      },
-    },
-    preferences: {
-      theme: "light",
-      notifications: {
-        email: false,
-        sms: true,
-      },
-    },
-  },
-  {
-    id: 3,
-    name: "John Doe",
-    email: "john.doe@example.com",
-    profile: {
-      age: 30,
-      gender: "other",
-      location: {
-        city: "Lalitpur",
-        country: "Nepal",
-      },
-    },
-    preferences: {
-      theme: "system",
-      notifications: {
-        email: true,
-        sms: true,
-      },
-    },
-  },
-];
-
-type User = typeof users[0];
-
-const getUsersProperty = <T extends User, K extends keyof T> (users:T[], key: K)  =>{
-    return users.map((user)=>(user[key]))
+function printObj(student: IStudent) {
+  console.log(student);
 }
 
-const usersOfOnlyKey = getUsersProperty(users,"name")
-console.log(usersOfOnlyKey);
+function merge<T, K>(obj1: T, obj2: K): T & K {
+  return {
+    ...obj1,
+    ...obj2,
+  };
+}
+
+class DataStorage<T> {
+  private storage: Array<T>;
+
+  constructor() {
+    this.storage = new Array();
+  }
+
+  public addItems(item: T) {
+    this.storage.push(item);
+  }
+
+  public removeItem(item: T) {
+    this.storage = this.storage.filter((i) => i !== item);
+  }
+
+  public getItems() {
+    return this.storage;
+  }
+}
+
+const dataStorage = new DataStorage<number>();
+dataStorage.addItems(5);
+dataStorage.addItems(Math.floor(Math.random() * "bishal".length));
+// console.log(dataStorage.getItems());
+
+const createKeyValuePair = <T extends string, K extends any>(a: T, b: K) => {
+  return {
+    [a]: b,
+  };
+};
+// console.log(createKeyValuePair("age",true));
+
+function mapArray<T, K>(a: T[], callback: (item:T) => K): K[] {
+  return a.map(callback)
+
+}
+
+ mapArray<string,number>(["bishal","hari","sita","gita"],(item)=>item.length)
+
+ type IsString<T> = T extends string ? "Yes": "No";
